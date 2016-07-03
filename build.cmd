1c6558b42393891c1e813ca2950035b4322e6bdb
Madhu <madhusudhana.kondragunta@gmail.com>
2016-07-03 10:36:55 +0530

    add cf templte

--- build.cmd
+++ build.cmd
@@ -2,5 +2,4 @@ echo "Demo GOCD"
 
 echo "Demo to Vijay"
 
-
-aws cloudformation list-stacks
+aws cloudformation create-stack --stack-name myteststack --template-url https://s3-us-west-2.amazonaws.com/nikedtr-dev/cftemplate.json
